176b141c6414e1ac5d41a60bb5e806807ffc99de
Tomas Barta <tmbrta@gmail.com>
2021-02-17 15:20:11 +0100

    modified gamma to work with scipy.stats

--- pointproc/densities.py
+++ pointproc/densities.py
@@ -1,6 +1,5 @@
 import numpy as np
-from scipy.special import gamma, gammainc
-from scipy.stats import norm, invgauss
+from scipy.stats import gamma, invgauss
 
 
 class DensityFunction:
@@ -31,13 +30,15 @@ class PoissonDensity(DensityFunction):
 class GammaDensity(DensityFunction):
     def __init__(self, init=1, bounds=(1e-3, None)):
         def function(intensity, int_intensity_diff, shape):
-            x1 = shape * intensity / gamma(shape)
-            x2 = (shape * int_intensity_diff) ** (shape - 1)
-            x3 = np.exp(-shape * int_intensity_diff)
-            return x1 * np.nan_to_num(x2) * x3
+            return intensity * gamma(x=int_intensity_diff, a=shape)
+            # x1 = shape * intensity / gamma(shape)
+            # x2 = (shape * int_intensity_diff) ** (shape - 1)
+            # x3 = np.exp(-shape * int_intensity_diff)
+            # return x1 * np.nan_to_num(x2) * x3
 
         def integral(int_intensity_diff, shape):
-            return gammainc(shape, int_intensity_diff)
+            return gamma.cdf(a=shape, x=int_intensity_diff)
+            # return gammainc(shape, int_intensity_diff)
 
         super(GammaDensity, self).__init__(function, integral, [init], [bounds])
 
